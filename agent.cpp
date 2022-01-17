a03b5c7b85a341ce46e81875f344eb851fa98850
smzttk432 <smzttkdrgn@gmail.com>
2022-01-17 09:59:11 +0900

    add agent.cpp

---
+++ agent.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+#include "agent.h"
+
+#include "tiny_dnn/tiny_dnn.h"
+
+void agent::setposition(float x, float y) {
+    this->position = { x,y };
+}
+float agent::dist_goal() {
+    return sqrt(pow(this->position[0] - GOALX, 2) + pow(this->position[1] - GOALY, 2));
+}
+void agent::move(int action) {
+    switch (action) {
+    case 0:
+        this->position[0] += 1;
+        break;
+    case 1:
+        this->position[0] -= 1;
+        break;
+    case 2:
+        this->position[1] += 1;
+        break;
+    case 3:
+        this->position[1] -= 1;
+        break;
+    }
+}
+
+tiny_dnn::vec_t agent::now_pos() {
+    return this->position;
+}
+
+void InitRand()
+{
+    srand((unsigned int)time(NULL));
+}
+
+int random_int(int max) {
+    srand((unsigned int)time(NULL));
+    return rand() % max;
+}
+
+float compute_reward(float bdist, float ndist, tiny_dnn::vec_t pos) {
+    if (ndist == 0) {
+        return 1.0;
+    }
+    else if (pos[0] < 0 || pos[1] < 0 || pos[0] >= MAXX || pos[1] >= MAXY) {
+        return -1.0;
+    }
+    else {
+        return bdist - ndist;
+    }
+}
+
+int act_by_net(tiny_dnn::vec_t result) {
+    float max = -2;
+    int i, a = 0;
+    for (i = 0; i < 4; i++) {
+        if (result[i] > max) {
+            a = i;
+            max = result[i];
+        }
+    }
+    return a;
+}
+
+tiny_dnn::vec_t rewards_vec(int action, float reward) {
+    tiny_dnn::vec_t rewards = { 0,0,0,0 };
+    rewards[action] = reward;
+    return rewards;
+}
+
+int learn_end(tiny_dnn::vec_t pos, int gen) {
+    if (pos[0] < 0 || pos[1] < 0 || gen == GENMAX || pos[0] >= MAXX || pos[1] >= MAXY || (pos[0] == GOALX && pos[1] == GOALY)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+int check_goal(tiny_dnn::vec_t pos, int gen) {
+    if ((pos[0] == GOALX && pos[1] == GOALY)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+tiny_dnn::vec_t agent::pos_vec() {
+    tiny_dnn::vec_t pos(MAXX * MAXY, 0);
+    float x = this->position[0];
+    float y = this->position[1];
+    pos[(x * MAXX) + y] = (float)1.0;
+    return pos;
+}
