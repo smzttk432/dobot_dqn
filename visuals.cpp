7b3e464a934f856197167850e630aea9486dc6b1
smzttk432 <smzttkdrgn@gmail.com>
2022-01-18 11:14:10 +0900

    visuals

--- visuals.cpp
+++ visuals.cpp
@@ -1,1 +1,5 @@
-#include 
+#define _CRT_SECURE_NO_WARNINGS
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include <stdio.h>
+#include "visuals.h"
--- visuals.h
+++ visuals.h
@@ -1,1 +1,4 @@
 #pragma once
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include <stdio.h>
