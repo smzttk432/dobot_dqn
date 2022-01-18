8a5c3d0f6327142d5c4d7b50f7f3eebe972bdc5c
smzttk432 <smzttkdrgn@gmail.com>
2022-01-18 11:25:08 +0900

    add binarize func

--- visuals.cpp
+++ visuals.cpp
@@ -3,3 +3,12 @@
 #include "opencv2/imgproc.hpp"
 #include <stdio.h>
 #include "visuals.h"
+
+
+void binarize(cv::Mat frame, visuals* vis, int valhmin, int valsmin, int valvmin, int valhmax, int valsmax, int valvmax) {
+    vis->dst = cv::Mat(frame, cv::Rect(80, 0, 480, 480));
+    cvtColor(vis->dst, vis->hsv, cv::COLOR_BGR2HSV);
+    cv::inRange(vis->hsv, cv::Scalar(valhmin, valsmin, valvmin), cv::Scalar(valhmax, valsmax, valvmax), vis->mask);
+    cv::minMaxLoc(vis->mask, &vis->min, &vis->max);
+    cv::bitwise_and(vis->dst, vis->dst, vis->res, vis->mask);
+}
--- visuals.h
+++ visuals.h
@@ -3,3 +3,12 @@
 #include "opencv2/imgproc.hpp"
 #include <stdio.h>
 
+struct visuals {
+    double min, max;
+    cv::Mat frame, hsv, mask, res, dst;
+    cv::Mat labelImage, statres, centroidsres;
+    cv::Point grav;
+    int nLabels;
+};
+
+void binarize(cv::Mat frame, visuals* vis, int valhmin, int valsmin, int valvmin, int valhmax, int valsmax, int valvmax);
